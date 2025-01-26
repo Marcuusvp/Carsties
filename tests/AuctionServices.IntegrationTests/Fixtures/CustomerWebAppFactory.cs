b39e134a4f7c74b867f861c93a57100c791639ba
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-01-26 16:57:09 -0300

    configurando serviços para teste de integracao

--- tests/AuctionServices.IntegrationTests/Fixtures/CustomerWebAppFactory.cs
+++ tests/AuctionServices.IntegrationTests/Fixtures/CustomerWebAppFactory.cs
@@ -1,10 +1,47 @@
+using AuctionServices.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
 
 namespace AuctionServices.IntegrationTests.Fixtures;
 
-public class CustomerWebAppFactory : WebApplicationFactory<Program>
+public class CustomerWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    
+    private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {   // Configurando projeto para remover o uso do banco e usar o container de teste
+            var descriptor = services.SingleOrDefault(d => 
+                d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+            if (descriptor != null) services.Remove(descriptor);
+            
+            services.AddDbContext<AuctionDbContext>(options =>
+            {
+                options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+            });
+            // Configurando o MassTransit para usar o teste de massa
+            services.AddMassTransitTestHarness();
+            // Criando o banco de dados de teste
+            var sp = services.BuildServiceProvider();
+            
+            using var scope = sp.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<AuctionDbContext>();
+            
+            db.Database.Migrate();
+        });
+    }
+    Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
 }
 
 
