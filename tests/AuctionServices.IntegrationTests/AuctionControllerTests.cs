3fe2523417e128102ad06cf06956a450ca204944
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-01-26 18:49:01 -0300

    fechando testes integrados de metodos GET

--- tests/AuctionServices.IntegrationTests/AuctionControllerTests.cs
+++ tests/AuctionServices.IntegrationTests/AuctionControllerTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using AuctionServices.Data;
 using AuctionServices.DTOs;
@@ -11,7 +12,7 @@ public class AuctionControllerTests : IClassFixture<CustomerWebAppFactory>, IAsy
 {
     private readonly CustomerWebAppFactory _factory;
     private readonly HttpClient _httpClient;
-
+    private const string GT_ID = "afbee524-5972-4075-8800-7d1f9d7b0a0c";
     public AuctionControllerTests(CustomerWebAppFactory factory)
     {
         _factory = factory;
@@ -26,6 +27,38 @@ public class AuctionControllerTests : IClassFixture<CustomerWebAppFactory>, IAsy
         var response = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("api/auctions");
         // Assert
         Assert.Equal(3, response.Count);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithValidId_ShouldReturnAuction()
+    {
+        // Arrange
+        
+        // Act
+        var response = await _httpClient.GetFromJsonAsync<AuctionDto>($"api/auctions/{GT_ID}");
+        
+        // Assert
+        Assert.Equal("GT", response.Model);
+    }
+    
+    [Fact]
+    public async Task GetAuctionById_WithInvalidId_ShouldReturnNotFound()
+    {
+        // Arrange
+        // Act
+        var response = await _httpClient.GetAsync($"api/auctions/{Guid.NewGuid()}");
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task GetAuctionById_WithInvalidGuid_ShouldReturnBadRequest()
+    {
+        // Arrange
+        // Act
+        var response = await _httpClient.GetAsync($"api/auctions/notAGuid");
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
     
     public Task InitializeAsync() => Task.CompletedTask;
