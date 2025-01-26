585a387e63758c9fb097ad0bd4e69fcf83819b66
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-01-26 19:27:14 -0300

    adicionando testes ao service bus

---
+++ tests/AuctionServices.IntegrationTests/AuctionBusTests.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using System.Net.Http.Json;
+using AuctionServices.Data;
+using AuctionServices.DTOs;
+using AuctionServices.IntegrationTests.Fixtures;
+using AuctionServices.IntegrationTests.Util;
+using Contracts;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionServices.IntegrationTests;
+
+public class AuctionBusTests : IClassFixture<CustomerWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomerWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    private ITestHarness _testHarness;
+    
+    public AuctionBusTests(CustomerWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = _factory.CreateClient();
+        _testHarness = factory.Services.GetTestHarness();
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithValidObject_ShouldPublishAuctionCreated()
+    {
+        // Arrange
+        var auction = GetAuctionForCreate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+        
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("api/auctions", auction);
+        
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.True(await _testHarness.Published.Any<AuctionCreated>());
+    }
+    
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+        
+        DbHelper.ReinitDbForTests(db);
+        return Task.CompletedTask;
+    }
+
+    private CreateAuctionDto GetAuctionForCreate()
+    {
+        return new CreateAuctionDto
+        {
+            Make = "test",
+            Model = "testModel",
+            ImageUrl = "test",
+            Color = "test",
+            Mileage = 10,
+            Year = 10,
+            ReservePrice = 10
+        };
+    }
+}
