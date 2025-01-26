f15b2f9eb33dd08118633064baf4e2fecd99431c
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-01-26 11:12:56 -0300

    criando o mock

---
+++ tests/AuctionServicesTest/AuctionControllerTests.cs
@@ -0,0 +1,31 @@
+using AuctionServices.Controllers;
+using AuctionServices.Data;
+using AuctionServices.RequestHelpers;
+using AutoFixture;
+using AutoMapper;
+using MassTransit;
+using Moq;
+
+namespace AuctionServicesTest;
+
+public class AuctionControllerTests
+{
+    private readonly Mock<IAuctionRepository> _auctionRepo;
+    private readonly Mock<IPublishEndpoint> _publishEndpoint;
+    private readonly Fixture _fixture;
+    private readonly AuctionsController _controller;
+    private readonly IMapper _mapper;
+    
+    public AuctionControllerTests()
+    {
+        _auctionRepo = new Mock<IAuctionRepository>();
+        _publishEndpoint = new Mock<IPublishEndpoint>();
+        _fixture = new Fixture();
+        var mockMapper = new MapperConfiguration(mc =>
+        {
+            mc.AddMaps(typeof(MappingProfiles).Assembly);
+        }).CreateMapper().ConfigurationProvider;
+        _mapper = new Mapper(mockMapper);
+        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+}
