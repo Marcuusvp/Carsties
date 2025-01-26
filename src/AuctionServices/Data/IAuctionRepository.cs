1eab66192770ea094b3ce2ff5e6f28a34b5a20e6
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-01-26 10:53:39 -0300

    Iniciando testes e refatorando código para melhorar a testabilidade

---
+++ src/AuctionServices/Data/IAuctionRepository.cs
@@ -0,0 +1,14 @@
+using AuctionServices.DTOs;
+using AuctionServices.Entities;
+
+namespace AuctionServices.Data;
+
+public interface IAuctionRepository
+{
+    Task<List<AuctionDto>> GetAucionsAsync(string date);
+    Task<AuctionDto> GetAuctionByIdAsync(Guid id);
+    Task<Auction> GetAuctionsEntityById(Guid id);
+    void AddAuction(Auction auction);
+    void RemoveAuction(Auction auction);
+    Task<bool> SaveChangesAsync();
+}
