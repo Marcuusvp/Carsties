2ce3df094943a15040c5efef0c3f1e9aab00dbe5
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-01-26 17:09:08 -0300

    Abstraindo o código para melhor leitura e entendimento

--- tests/AuctionServices.IntegrationTests/Fixtures/CustomerWebAppFactory.cs
+++ tests/AuctionServices.IntegrationTests/Fixtures/CustomerWebAppFactory.cs
@@ -1,4 +1,5 @@
 using AuctionServices.Data;
+using AuctionServices.IntegrationTests.Util;
 using MassTransit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -21,24 +22,18 @@ public class CustomerWebAppFactory : WebApplicationFactory<Program>, IAsyncLifet
     {
         builder.ConfigureTestServices(services =>
         {   // Configurando projeto para remover o uso do banco e usar o container de teste
-            var descriptor = services.SingleOrDefault(d => 
-                d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
-            if (descriptor != null) services.Remove(descriptor);
+            services.RemoveDbContext<AuctionDbContext>();
             
             services.AddDbContext<AuctionDbContext>(options =>
             {
                 options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
             });
+            
             // Configurando o MassTransit para usar o teste de massa
             services.AddMassTransitTestHarness();
+            
             // Criando o banco de dados de teste
-            var sp = services.BuildServiceProvider();
-            
-            using var scope = sp.CreateScope();
-            var scopedServices = scope.ServiceProvider;
-            var db = scopedServices.GetRequiredService<AuctionDbContext>();
-            
-            db.Database.Migrate();
+            services.EnsureCreated<AuctionDbContext>();
         });
     }
     Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
