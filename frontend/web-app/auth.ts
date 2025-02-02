a40811ec457616a604ae95436ec2f29e274ae842
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-02 00:02:19 -0300

    Configurando session data

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -14,6 +14,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             issuer: "http://172.18.38.169:5000",
             authorization: {params: {scope: 'openid profile auctionApp'}},
             idToken: true
-        } as OIDCConfig<Profile>)
+        } as OIDCConfig<Omit<Profile, 'username'>>)
     ],
+    callbacks: {
+        async jwt({token, profile}) {
+            // console.log({token, user, account, profile})
+            if(profile){
+                token.username = profile.username
+            }
+            return token;
+        },
+        async session({session, token}){
+            // console.log({session, token})
+            if(token){
+                session.user.username = token.username
+            }
+            return session;
+        }
+    }
 })
