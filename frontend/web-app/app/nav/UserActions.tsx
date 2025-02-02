f44920cd6766b7d5930d42e7231fb59e425b4ac7
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-02 00:15:46 -0300

    Adicionando funcionalidade de logout, dropdowns e ajustes de router

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,11 +1,38 @@
-import { Button } from 'flowbite-react'
-import Link from 'next/link'
-import React from 'react'
+"use client"
 
-export default function UserActions() {
+import { Dropdown, DropdownDivider, DropdownItem } from 'flowbite-react'
+import { User } from 'next-auth'
+import { signOut } from 'next-auth/react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
+import { HiCog, HiUser } from 'react-icons/hi2'
+
+type Props = {
+  user: User
+}
+
+export default function UserActions({user}: Props) {
+  const router = useRouter();
   return (
-    <Button outline>
-        <Link href='/session'>Session</Link>
-    </Button>
+    <Dropdown inline label={`Bem-vindo ${user.name}`}>
+      <DropdownItem icon={HiUser}>
+        <Link href='/'>My Auctions</Link>
+      </DropdownItem>
+      <DropdownItem icon={AiFillTrophy}>
+        <Link href='/'>Auctions Won</Link>
+      </DropdownItem>
+      <DropdownItem icon={AiFillCar}>
+        <Link href='/'>Sell my car</Link>
+      </DropdownItem>
+      <DropdownItem icon={HiCog}>
+        <Link href='/session'>Session (dev only!)</Link>
+      </DropdownItem>
+      <DropdownDivider />
+      <DropdownItem icon={AiOutlineLogout} onClick={() => signOut({redirectTo: '/'})}>
+        Sign out
+      </DropdownItem>
+    </Dropdown>
   )
 }
