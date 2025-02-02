c1d647cfb56b3831d20b3e518ba425f9ba6f8717
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-02 17:02:06 -0300

    criando input personalizado

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -3,9 +3,10 @@
 import { Button, TextInput } from 'flowbite-react';
 import React from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
+import Input from '../components/input';
 
 export default function AuctionForm() {
-    const {register, handleSubmit, setFocus, formState: {isSubmitting, isValid, isDirty, errors}} = useForm();
+    const {control, handleSubmit, setFocus, formState: {isSubmitting, isValid, isDirty, errors}} = useForm();
 
     function onSubmit(data: FieldValues) {
         console.log(data);
@@ -13,22 +14,8 @@ export default function AuctionForm() {
 
     return (
         <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
-            <div className='mb-3 block'>
-                <TextInput 
-                    {...register('make', {required: 'Informe o fabricante'})}
-                    placeholder='Fabricante'
-                    color={errors?.make && 'failure'}
-                    helperText={errors.make?.message as string}
-                />
-            </div>
-            <div className='mb-3 block'>
-                <TextInput 
-                    {...register('model', {required: 'Informe o modelo do veículo'})}
-                    placeholder='Modelo'
-                    color={errors?.model && 'failure'}
-                    helperText={errors.model?.message as string}
-                />
-            </div>
+            <Input label='Fabricante' name='make' control={control} rules={{required: 'Informe o fabricante'}}/>
+            <Input label='Modelo' name='model' control={control} rules={{required: 'Informe o modelo'}}/>
             <div className='flex justify-between'>
                 <Button outline color='gray'>Cancel</Button>
                 <Button 
