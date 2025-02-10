368980e763a8d4dbacf9ed9d74afc1e957bfd161
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-10 20:12:01 -0300

    atualizando nosso gRPC Client para se comunicar com o Server

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using BiddingService.Services;
 using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authorization;
@@ -15,11 +16,13 @@ public class BidsController : ControllerBase
 {
     private readonly IMapper _mapper;
     private readonly IPublishEndpoint _publishEndpoint;
+    private readonly GrpcAuctionClient _grpcAuctionClient;
 
-    public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint)
+    public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint, GrpcAuctionClient grpcAuctionClient)
     {
         _mapper = mapper;
         _publishEndpoint = publishEndpoint;
+        _grpcAuctionClient = grpcAuctionClient;
     }
     [Authorize]
     [HttpPost]
@@ -29,8 +32,8 @@ public class BidsController : ControllerBase
 
         if (auction == null)
         {
-            // TODO: check with auction service if that has auction
-            return NotFound();
+            auction = _grpcAuctionClient.GetAuction(auctionId);
+            if (auction is null) return BadRequest("Cannot accept bids on this auction at this time");
         }
 
         if (auction.Seller == User.Identity.Name)
--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -40,6 +40,7 @@ builder.Services.AddHostedService<CheckAuctionFinished>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<GrpcAuctionClient>();
 
 var app = builder.Build();
 
