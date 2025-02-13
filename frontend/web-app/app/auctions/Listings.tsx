7e59fb0d57db46e73db6aa1e90f2f6e68803685c
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-13 20:26:37 -0300

    refatorando para usar o zustand no lugar de local state

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -10,11 +10,12 @@ import { useParamsStore } from '@/hooks/useParamsStore';
 import { useShallow } from 'zustand/shallow';
 import qs from 'query-string';
 import EmptyFilter from '../components/EmptyFilter';
+import { useAuctionsStore } from '@/hooks/useAuctionStore';
 
 
 
 export default function Listings() {
-  const [data, setData] = useState<PagedResult<Auction>>();
+  const [loading, setLoading] = useState(true);
   const params = useParamsStore(useShallow(state => ({
     pageNumber: state.pageNumber,
     pageSize: state.pageSize,
@@ -24,6 +25,13 @@ export default function Listings() {
     seller: state.seller,
     winner: state.winner,
   })))
+  const data = useAuctionsStore(useShallow(state => ({
+    auctions: state.auctions,
+    totalCount: state.totalCount,
+    pageCount: state.pageCount
+  })))
+const setData = useAuctionsStore(state => state.setData);
+
   const setParams = useParamsStore(state => state.setParams);
   const url = qs.stringifyUrl({url: '', query: params})
 
@@ -34,10 +42,11 @@ export default function Listings() {
   useEffect(() => {
     getData(url).then(data => {
       setData(data)
+      setLoading(false)
     })
   }, [url])
 
-  if(!data) return <h3>Loading...</h3>
+  if(loading) return <h3>Loading...</h3>
 
   return (
     <>
@@ -47,7 +56,7 @@ export default function Listings() {
       ) : (
         <>
           <div className='grid grid-cols-4 gap-6'>
-            {data.results.map((auction) => (
+            {data.auctions.map((auction) => (
               <AuctionCard key={auction.id} auction={auction}/>
             ))}
           </div>
