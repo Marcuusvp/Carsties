45c30aa0aeb15f1279c9ba380e3d364fbb5e6c4c
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-16 17:50:34 -0300

    adicionando notificacao em real time para a criacao de uma nova auction

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -2,20 +2,30 @@
 
 import { useAuctionsStore } from '@/hooks/useAuctionStore'
 import { useBidStore } from '@/hooks/useBidStore'
-import { Bid } from '@/types'
+import { Auction, Bid } from '@/types'
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
+import { User } from 'next-auth'
 import { useParams } from 'next/navigation'
-import React, { ReactNode, useCallback, useEffect, useRef } from 'react'
+import { ReactNode, useCallback, useEffect, useRef } from 'react'
+import toast from 'react-hot-toast'
+import AuctionCreatedToast from '../components/AuctionCreatedToast'
 
 type Props = {
     children: ReactNode
+    user: User | null
 }
 
-export default function SignalRProvider({children}: Props) {
+export default function SignalRProvider({children, user}: Props) {
     const connection = useRef<HubConnection | null>(null);
     const setCurrentPrice = useAuctionsStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
     const params = useParams<{id: string}>();
+
+    const handleAuctionCreated = useCallback((auction: Auction) => {
+        if(user?.username !== auction.seller){
+            return toast(<AuctionCreatedToast auction={auction}/>, {duration: 5000})
+        }
+    }, [user?.username])
 
     const handleBidPlaced = useCallback((bid: Bid) => {
         if(bid.bidStatus.includes('Accepted')){
@@ -31,16 +41,18 @@ export default function SignalRProvider({children}: Props) {
         if (!connection.current){
             connection.current = new HubConnectionBuilder().withUrl('http://localhost:6001/notifications').withAutomaticReconnect().build();
 
-            connection.current.start().then(() => 'Connected to notification hub').catch(err => console.log(err));
+            connection.current.start().then(() => 'Connected to notification hub').catch(err => console.log(err));            
         }
 
         connection.current.on('BidPlaced', handleBidPlaced)
+        connection.current.on('AuctionCreated', handleAuctionCreated)
 
         return () => {
             connection.current?.off('BidPlaced', handleBidPlaced)
+            connection.current?.off('AuctionCreated', handleAuctionCreated)
         }
 
-    }, [setCurrentPrice, handleBidPlaced])
+    }, [setCurrentPrice, handleBidPlaced, handleAuctionCreated])
 
     return (
         children
