45f82628e1060757c039fb7cf943410a1faeb1bb
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-02-18 19:17:44 -0300

    mais uns ajustes

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -28,11 +28,8 @@ internal static class HostingExtensions
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
-
-                if (builder.Environment.IsEnvironment("Docker"))
-                {
-                    options.IssuerUri = builder.Configuration["IssuerUri"] ?? "http://localhost:5000";
-                }
+                options.IssuerUri = builder.Configuration["IssuerUri"] ?? "http://localhost:5000";
+                
                 // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
                 // options.EmitStaticAudienceClaim = true;
             })
